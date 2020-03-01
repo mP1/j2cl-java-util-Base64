2bd8fde46a68074638ec596dac9bb74887c7c53c
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2020-03-02 09:53:57 +1100

    Encoder

--- src/main/java/walkingkooka/javautilbase64j2cl/java/util/Base64.java
+++ src/main/java/walkingkooka/javautilbase64j2cl/java/util/Base64.java
@@ -18,10 +18,17 @@
 
 package walkingkooka.javautilbase64j2cl.java.util;
 
+import walkingkooka.NeverError;
 import walkingkooka.reflect.PublicStaticHelper;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
+/**
+ * <a href="https://tools.ietf.org/html/rfc4648"></a>
+ */
 public final class Base64 implements PublicStaticHelper {
 
     public static Base64.Encoder getEncoder() {
@@ -36,8 +43,28 @@ public final class Base64 implements PublicStaticHelper {
         return Base64.Encoder.RFC2045;
     }
 
-    public static Base64.Encoder getMimeEncoder(int lineLength, byte[] lineSeparator) {
-        throw new UnsupportedOperationException();
+    public static Base64.Encoder getMimeEncoder(final int lineLength,
+                                                final byte[] lineSeparator) {
+        Objects.requireNonNull(lineSeparator);
+
+        return lineLength / 4 <= 0 ?
+                Encoder.RFC4648 :
+                getMimeEncoder0(lineLength, lineSeparator);
+    }
+
+    private static Base64.Encoder getMimeEncoder0(final int lineLength,
+                                                  final byte[] lineSeparator) {
+        int at = 0;
+        for (final byte value : lineSeparator) {
+            if (-1 != RFC4648_ALPHABET.indexOf(value)) {
+                throw new IllegalArgumentException("Illegal base64 line separator at " + at + " character 0x" + Integer.toString(value, 16));
+            }
+            at++;
+        }
+        return new Base64.Encoder(RFC4648_ALPHABET_CHARS,
+                        lineLength / 4 * 4,
+                        lineSeparator,
+                        Base64EncoderPadding.WITH);
     }
 
     public static Decoder getDecoder() {
@@ -52,24 +79,187 @@ public final class Base64 implements PublicStaticHelper {
         return Decoder.RFC2045;
     }
 
+    /**
+     * <pre>
+     *                       Table 1: The Base 64 Alphabet
+     *
+     *      Value Encoding  Value Encoding  Value Encoding  Value Encoding
+     *          0 A            17 R            34 i            51 z
+     *          1 B            18 S            35 j            52 0
+     *          2 C            19 T            36 k            53 1
+     *          3 D            20 U            37 l            54 2
+     *          4 E            21 V            38 m            55 3
+     *          5 F            22 W            39 n            56 4
+     *          6 G            23 X            40 o            57 5
+     *          7 H            24 Y            41 p            58 6
+     *          8 I            25 Z            42 q            59 7
+     *          9 J            26 a            43 r            60 8
+     *         10 K            27 b            44 s            61 9
+     *         11 L            28 c            45 t            62 +
+     *         12 M            29 d            46 u            63 /
+     *         13 N            30 e            47 v
+     *         14 O            31 f            48 w         (pad) =
+     *         15 P            32 g            49 x
+     *         16 Q            33 h            50 y
+     * </pre>
+     */
+    private final static String RFC4648_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    private final static char[] RFC4648_ALPHABET_CHARS = RFC4648_ALPHABET.toCharArray();
+
+    /**
+     * <pre>
+     *          Table 2: The "URL and Filename safe" Base 64 Alphabet
+     *
+     *      Value Encoding  Value Encoding  Value Encoding  Value Encoding
+     *          0 A            17 R            34 i            51 z
+     *          1 B            18 S            35 j            52 0
+     *          2 C            19 T            36 k            53 1
+     *          3 D            20 U            37 l            54 2
+     *          4 E            21 V            38 m            55 3
+     *          5 F            22 W            39 n            56 4
+     *          6 G            23 X            40 o            57 5
+     *          7 H            24 Y            41 p            58 6
+     *          8 I            25 Z            42 q            59 7
+     *          9 J            26 a            43 r            60 8
+     *         10 K            27 b            44 s            61 9
+     *         11 L            28 c            45 t            62 - (minus)
+     *         12 M            29 d            46 u            63 _
+     *         13 N            30 e            47 v           (underline)
+     *         14 O            31 f            48 w
+     *         15 P            32 g            49 x
+     *         16 Q            33 h            50 y         (pad) =
+     * </pre>
+     */
+    private final static char[] RFC4648_URLSAFE_ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_".toCharArray();
+
+    private static final int MIMELINEMAX = 76;
+    private static final byte[] CRLF = new byte[]{'\r', '\n'};
+
+    private static final byte MASK = 0x3f;
+    static final byte PAD = '=';
+
     public static class Encoder {
 
-        final static Encoder RFC4648 = new Encoder();
-        final static Encoder RFC4648_URLSAFE = new Encoder();
-        final static Encoder RFC2045 = new Encoder();
+        final static Encoder RFC4648 = new Encoder(RFC4648_ALPHABET_CHARS, -1, new byte[0], Base64EncoderPadding.WITH);
+        final static Encoder RFC4648_URLSAFE = new Encoder(RFC4648_URLSAFE_ALPHABET, -1, new byte[0], Base64EncoderPadding.WITH);
+        final static Encoder RFC2045 = new Encoder(RFC4648_ALPHABET_CHARS, MIMELINEMAX, CRLF, Base64EncoderPadding.WITH);
 
-        private Encoder() {
+        private Encoder(final char[] alphabet,
+                        final int maxLineLength,
+                        final byte[] separator,
+                        final Base64EncoderPadding padding) {
             super();
+            this.alphabet = alphabet;
+            this.maxLineLength = maxLineLength;
+            this.separator = separator;
+            this.padding = padding;
         }
 
-        public byte[] encode(final byte[] src) {
-            throw new UnsupportedOperationException();
+        /**
+         * <pre>
+         * 9.  Illustrations and Examples
+         *
+         *    To translate between binary and a base encoding, the input is stored
+         *    in a structure, and the output is extracted.  The case for base 64 is
+         *    displayed in the following figure, borrowed from [5].
+         *
+         *             +--first octet--+-second octet--+--third octet--+
+         *             |7 6 5 4 3 2 1 0|7 6 5 4 3 2 1 0|7 6 5 4 3 2 1 0|
+         *             +-----------+---+-------+-------+---+-----------+
+         *             |5 4 3 2 1 0|5 4 3 2 1 0|5 4 3 2 1 0|5 4 3 2 1 0|
+         *             +--1.index--+--2.index--+--3.index--+--4.index--+
+         * </pre>
+         */
+        public byte[] encode(final byte[] from) {
+            Objects.requireNonNull(from, "from");
+
+            try (final ByteArrayOutputStream bytes = new ByteArrayOutputStream()) {
+
+                final int fromLength = from.length;
+                final int maxLineLength = this.maxLineLength;
+                final char[] alphabet = this.alphabet;
+                final Base64EncoderPadding padding = this.padding;
+                final byte[] separator = this.separator;
+
+                int lineWidth = 0;
+                int offset = 0;
+                int previous = 0;
+
+                for (int i = 0; i < fromLength; i++) {
+                    final int value = from[i] & 0xFF;
+
+                    switch (offset) {
+                        case 0:
+                            if (lineWidth == maxLineLength) {
+                                padding.writeSeparator(bytes, separator);
+                                lineWidth = 0;
+                            }
+
+                            bytes.write(alphabet[value >>> 2]);
+                            previous = (value & 0x3) << 4;
+                            offset = 1;
+                            lineWidth++;
+                            break;
+                        case 1:
+                            bytes.write(alphabet[previous | (value >>> 4)]);
+                            previous = (value & 0xf) << 2;
+                            offset = 2;
+                            lineWidth++;
+                            break;
+                        case 2:
+                            bytes.write(alphabet[previous | (value >>> 6)]);
+                            bytes.write(alphabet[value & MASK]);
+                            previous = 0;
+                            offset = 0;
+                            lineWidth++;
+                            lineWidth++;
+                            break;
+                        default:
+                            NeverError.unhandledCase(offset, 0, 1, 2);
+                            break;
+                    }
+                }
+
+                switch (offset) {
+                    case 0:
+                        break;
+                    case 1:
+                        bytes.write(alphabet[previous]);
+                        lineWidth += 1 + padding.write1(bytes);
+                        break;
+                    case 2:
+                        bytes.write(alphabet[previous]);
+
+                        lineWidth += 1 + padding.write2(bytes);
+                        break;
+                    default:
+                        NeverError.unhandledCase(offset, 0, 1, 2);
+                        break;
+                }
+
+                bytes.flush();
+                return bytes.toByteArray();
+            } catch (final IOException cause) {
+                throw new Error(cause.getMessage(), cause); // shouldnt happen.
+            }
         }
 
-        public int encode(final byte[] src,
-                          final byte[] dst) {
-            throw new UnsupportedOperationException();
+        public int encode(final byte[] from,
+                          final byte[] to) {
+            final byte[] encoded = this.encode(from);
+            final int length = encoded.length;
+
+            if (to.length < length) {
+                throw new IllegalArgumentException("To " + to.length + " < required " + length);
+            }
+            System.arraycopy(encoded, 0, to, 0, length);
+            return length;
         }
+
+        private final char[] alphabet;
+        private final int maxLineLength;
+        private final byte[] separator;
+        private final Base64EncoderPadding padding;
 
         @SuppressWarnings("deprecation")
         public String encodeToString(final byte[] src) {
@@ -78,18 +268,39 @@ public final class Base64 implements PublicStaticHelper {
         }
 
         public Encoder withoutPadding() {
-            throw new UnsupportedOperationException();
+            return Base64EncoderPadding.WITHOUT == this.padding ?
+                    this :
+                    new Encoder(this.alphabet, this.maxLineLength, this.separator, Base64EncoderPadding.WITHOUT);
+        }
+
+        @Override
+        public String toString() {
+            final String toString;
+
+            if (RFC4648_ALPHABET_CHARS == this.alphabet) {
+                final int max = this.maxLineLength;
+                if (-1 == max) {
+                    toString = "RFC4648" + this.padding;
+                } else {
+                    toString = "RFC2045 lineWidth=" + max + this.padding;
+                }
+            } else {
+                toString = "RFC4648 URLSAFE" + this.padding;
+            }
+
+            return toString;
         }
     }
 
     public static class Decoder {
 
-        final static Decoder RFC4648 = new Decoder();
-        final static Decoder RFC4648_URLSAFE = new Decoder();
-        final static Decoder RFC2045 = new Decoder();
+        final static Decoder RFC4648 = new Decoder(RFC4648_ALPHABET_CHARS);
+        final static Decoder RFC4648_URLSAFE = new Decoder(RFC4648_URLSAFE_ALPHABET);
+        final static Decoder RFC2045 = new Decoder(RFC4648_ALPHABET_CHARS);
 
-        private Decoder() {
+        private Decoder(final char[] alphabet) {
             super();
+            this.alphabet = alphabet;
         }
 
         public byte[] decode(final byte[] src) {
@@ -104,6 +315,8 @@ public final class Base64 implements PublicStaticHelper {
                           final byte[] dst) {
             throw new UnsupportedOperationException();
         }
+
+        private final char[] alphabet;
     }
 
     /**
--- src/test/java/walkingkooka/javautilbase64j2cl/java/util/Base64Test.java
+++ src/test/java/walkingkooka/javautilbase64j2cl/java/util/Base64Test.java
@@ -17,12 +17,255 @@
 
 package walkingkooka.javautilbase64j2cl.java.util;
 
+import org.junit.jupiter.api.Test;
+import walkingkooka.ToStringTesting;
+import walkingkooka.javautilbase64j2cl.java.util.Base64.Encoder;
 import walkingkooka.reflect.JavaVisibility;
 import walkingkooka.reflect.PublicStaticHelperTesting;
+import walkingkooka.text.CharSequences;
 
 import java.lang.reflect.Method;
+import java.util.Arrays;
 
-public final class Base64Test implements PublicStaticHelperTesting<Base64> {
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class Base64Test implements PublicStaticHelperTesting<Base64>, ToStringTesting {
+
+    @Test
+    public void testWithoutPaddingWithoutPadding() {
+        final Encoder encoder = Base64.getEncoder();
+        final Encoder without = encoder.withoutPadding();
+        assertSame(without, without.withoutPadding());
+    }
+
+    @Test
+    public void testEncoderEncodeByteFromNullFails() {
+        final byte[] from = null;
+        assertThrows(NullPointerException.class, () -> java.util.Base64.getEncoder().encode(from));
+        assertThrows(NullPointerException.class, () -> Base64.getEncoder().encode(from));
+    }
+
+    @Test
+    public void testEncoderEncodeBytesBytesFromNullFails() {
+        final byte[] from = null;
+        final byte[] to = null;
+        assertThrows(NullPointerException.class, () -> java.util.Base64.getEncoder().encode(from, to));
+        assertThrows(NullPointerException.class, () -> Base64.getEncoder().encode(from, to));
+    }
+
+    @Test
+    public void testEncoderEncodeBytesBytesToNullFails() {
+        final byte[] from = new byte[5];
+        final byte[] to = null;
+        assertThrows(NullPointerException.class, () -> java.util.Base64.getEncoder().encode(from, to));
+        assertThrows(NullPointerException.class, () -> Base64.getEncoder().encode(from, to));
+    }
+
+    @Test
+    public void testEncoderEncodeBytesBytesToSmallFails() {
+        final byte[] from = new byte[5];
+        final byte[] to = new byte[6];
+        assertThrows(IllegalArgumentException.class, () -> java.util.Base64.getEncoder().encode(from, to));
+        assertThrows(IllegalArgumentException.class, () -> Base64.getEncoder().encode(from, to));
+    }
+
+    @Test
+    public void testEncodeByteEmpty() {
+        this.encodeAndCheck(0);
+    }
+
+    @Test
+    public void testEncodeByteOnce() {
+        this.encodeAndCheck(1);
+    }
+
+    @Test
+    public void testEncodeByteTwice() {
+        this.encodeAndCheck(2);
+    }
+
+    @Test
+    public void testEncodeByteThrice() {
+        this.encodeAndCheck(3);
+    }
+
+    @Test
+    public void testEncodeByteFour() {
+        this.encodeAndCheck(4);
+    }
+
+    @Test
+    public void testEncodeByteFive() {
+        this.encodeAndCheck(5);
+    }
+
+    @Test
+    public void testEncodeByte57MimeEncoder() {
+        final java.util.Base64.Encoder jdk = java.util.Base64.getMimeEncoder();
+        final Base64.Encoder emul = Base64.getMimeEncoder();
+
+        for (int i = Byte.MIN_VALUE; i <= Byte.MAX_VALUE; i++) {
+            final byte[] values = new byte[19 * 3];
+            final byte value = (byte) i;
+            Arrays.fill(values, value);
+
+            this.encodeAndCheck(jdk, emul, values);
+        }
+    }
+
+    @Test
+    public void testEncodeMimeEncoderLineLengthLineSeparator() {
+        for (int lineLength = 0; lineLength < 255; lineLength++) {
+            final byte[] separator = new byte[]{(byte) '\n', (byte) '\n', (byte) '\n'};
+
+            final java.util.Base64.Encoder jdk = java.util.Base64.getMimeEncoder(lineLength, separator);
+            final Base64.Encoder emul = Base64.getMimeEncoder(lineLength, separator);
+
+            for (int i = 0; i < 255; i++) {
+                final byte[] values = new byte[i];
+                final byte value = (byte) i;
+                Arrays.fill(values, value);
+
+                this.encodeAndCheck(jdk, emul, values);
+            }
+        }
+    }
+
+    @Test
+    public void testEncodeByteMany() {
+        for (int i = 0; i < 255; i++) {
+            this.encodeAndCheck(i);
+        }
+    }
+
+    @Test
+    public void testEncode() {
+        for (int i = 0; i < 255; i++) {
+            final byte[] values = new byte[i];
+
+            for (int j = 0; j < 255; j++) {
+                final byte value = (byte) i;
+                Arrays.fill(values, value);
+            }
+
+            this.encodeAndCheck(values);
+        }
+    }
+
+    private void encodeAndCheck(final int length) {
+        for (int i = Byte.MIN_VALUE; i <= Byte.MAX_VALUE; i++) {
+            this.encodeAndCheck((byte) i, length);
+        }
+    }
+
+    private void encodeAndCheck(final byte value,
+                                final int length) {
+        final byte[] values = new byte[length];
+        Arrays.fill(values, value);
+
+        this.encodeAndCheck(values);
+    }
+
+    // encode...........................................................................................................
+
+    private void encodeAndCheck(final byte[] values) {
+        this.encodeAndCheck(java.util.Base64.getEncoder(),
+                Base64.getEncoder(),
+                values);
+
+        this.encodeAndCheck(java.util.Base64.getEncoder().withoutPadding(),
+                Base64.getEncoder().withoutPadding(),
+                values);
+
+        this.encodeAndCheck(java.util.Base64.getMimeEncoder(),
+                Base64.getMimeEncoder(),
+                values);
+    }
+
+    private void encodeAndCheck(final java.util.Base64.Encoder jdk,
+                                final Base64.Encoder emul,
+                                final byte[] values) {
+        assertArrayEquals(jdk.encode(values),
+                emul.encode(values),
+                () -> emul + " encode(byte[]) " + Arrays.toString(values));
+        {
+            final byte[] encoded = new byte[values.length * 3 + 4];
+            final byte[] encoded2 = new byte[values.length * 3 + 4];
+
+            jdk.encode(values, encoded);
+            emul.encode(values, encoded2);
+
+            assertArrayEquals(encoded,
+                    encoded2,
+                    () -> emul + " encode(byte[], byte[]) " + Arrays.toString(values));
+
+            assertArrayEquals(encoded,
+                    encoded2,
+                    () -> emul + " encode(byte[], byte[]) without padding " + Arrays.toString(values));
+
+        }
+
+        assertEquals(jdk.encodeToString(values),
+                emul.encodeToString(values),
+                () -> emul + " encodeToString " + Arrays.toString(values));
+    }
+
+    // decode...........................................................................................................
+
+    private void decodeAndCheck(final java.util.Base64.Decoder jdk,
+                                final Base64.Decoder emul,
+                                final byte[] values) {
+        assertArrayEquals(jdk.decode(values),
+                emul.decode(values),
+                () -> "decode(byte[]) " + Arrays.toString(values));
+
+        final byte[] decoded = new byte[values.length];
+        final byte[] decoded2 = new byte[values.length];
+
+        jdk.decode(values, decoded);
+        emul.decode(values, decoded2);
+
+        assertArrayEquals(decoded,
+                decoded2,
+                () -> "decode(byte[], byte[]) " + Arrays.toString(values));
+
+
+        final String string = new String(values);
+        assertEquals(jdk.decode(string),
+                emul.decode(string),
+                () -> "decode " + CharSequences.quoteAndEscape(string));
+    }
+
+    // toString.........................................................................................................
+
+    @Override
+    public void testCheckToStringOverridden() {
+    }
+
+    @Test
+    public void testEncoderBasicToString() {
+        this.toStringAndCheck(Base64.getEncoder(), "RFC4648 WITH PADDING");
+    }
+
+    @Test
+    public void testEncoderBasicWithoutPaddingToString() {
+        this.toStringAndCheck(Base64.getEncoder().withoutPadding(), "RFC4648");
+    }
+
+    @Test
+    public void testEncoderMimeToString() {
+        this.toStringAndCheck(Base64.getMimeEncoder(), "RFC2045 lineWidth=76 WITH PADDING");
+    }
+
+    @Test
+    public void testEncoderMimeWithoutPaddingToString() {
+        this.toStringAndCheck(Base64.getMimeEncoder().withoutPadding(), "RFC2045 lineWidth=76");
+    }
+
+    // PublicStaticHelper...............................................................................................
 
     @Override
     public Class<Base64> type() {
